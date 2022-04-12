04a7d11b832836eea74a6a0476f41e777913d1cb
dfsartoph <98827908+dfsartoph@users.noreply.github.com>
2022-04-11 22:09:22 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -13,7 +13,7 @@ def main():
         dirs.remove(x) 
     for filenames in files:
       file_path = os.path.join(root, filename)
-      newDict[filename] = file_path
+      fileDict[filename] = file_path
 
 def hashing(file):
   hash_object = hashlib.new('sha256')
@@ -22,13 +22,16 @@ def hashing(file):
     hash_object.update(contents)
     with open('filelog.txt', 'wb') as g:
       g.write(hash_object.hexidigest())
-      g.close()
-    f.close()
+      
     
 
     
 if __name__=="__main__":
   main()
+  
+  
+## riptutorial.com/python/example/9666/file-hashing
+## docs.python.org/3/library/os.html#files-and-directories
     
       
    
