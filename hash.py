4b5f8ba047984f39dc5ab90d4dce4d3ba95bd315
dfsartoph <98827908+dfsartoph@users.noreply.github.com>
2022-04-11 22:58:51 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,8 +1,11 @@
 import hashlib
+import strings
 import os
 from os.path import join, getsize
+from os.stat import os.stat_result
+import glob
 unauthorized = ["/proc/", "/dev/", "/run/", "/sys/", "/tmp/", "/var/lib/", "/var/run/"]
-fileDict = {}
+fileList = []
 
 
 
@@ -11,19 +14,38 @@ def main():
     for x in dirs:
       if x in unauthorized:
         dirs.remove(x) 
-    for filenames in files:
+    for filename in files:
       file_path = os.path.join(root, filename)
-      fileDict[filename] = file_path
+      fileList.append((filename,file_path))
+      hashing(filename)
+  compare_files()
+   
 
 def hashing(file):
+  fileInfo = ""
   hash_object = hashlib.new('sha256')
   with open(file, 'rb') as f:
     contents = f.read()
     hash_object.update(contents)
-    with open('filelog.txt', 'wb') as g:
-      g.write(hash_object.hexidigest())
+    with open("log"+str(os.stat_result(st_mtime)+".txt", 'wb') as g:
+      for pair in fileList:
+        fileInfo += hash_object.hexidigest() + "," + pair[0] + "," + pair[1] + "\n"
+      g.write(fileInfo)
       
-    
+##def compare_files():
+  ## newList = sorted(glob.glob("log*.txt")):
+   ##with open(newList[-1], 'rb') as file1:
+     ## with open(newList[-2], 'rb') as file2:
+       ##    for x in file1.readlines():
+         ##     for y in file2.readlines():
+           ##     os.stat_result(
+             
+              
+              
+              
+              
+   
+
 
     
 if __name__=="__main__":
@@ -32,6 +54,7 @@ if __name__=="__main__":
   
 ## riptutorial.com/python/example/9666/file-hashing
 ## docs.python.org/3/library/os.html#files-and-directories
+## pythontutorial.net/python-basic/python-dictionary
     
       
    
