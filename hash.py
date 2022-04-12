819674071d2b95ec7f8825968787d8fe8f3c7ee5
dfsartoph <98827908+dfsartoph@users.noreply.github.com>
2022-04-11 23:25:40 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -21,23 +21,26 @@ def main(): ## this function will iterate through the directories, call the hash
    
 
 def hashing(file): ## this function will hash each file and add file hash, filename, and file path into a log file 
-  fileInfo = ""
+  fileInfo = []
   hash_object = hashlib.new('sha256')
   with open(file, 'rb') as f:
     contents = f.read()
     hash_object.update(contents)
     with open("log"+str(os.stat_result(st_mtime)+".txt", 'wb') as g:
       for pair in fileList:
-        fileInfo += hash_object.hexidigest() + "," + pair[0] + "," + pair[1] + "\n"
+        fileInfo.append((hash_object.hexidigest(),pair[0],pair[1]))
       g.write(fileInfo)
       
-##def compare_files(): ## this function will compare the most recent file, with the second most recent file, and print out summary information INCOMPLETE
-  ## newList = sorted(glob.glob("log*.txt")):
-   ##with open(newList[-1], 'rb') as file1:
-     ## with open(newList[-2], 'rb') as file2:
-       ##    for x in file1.readlines():
-         ##     for y in file2.readlines():
-           ##     os.stat_result(
+def compare_files(): ## this function will compare the most recent file, with the second most recent file, and print out summary information INCOMPLETE
+  summaryInfo = ""
+  newList = sorted(glob.glob("log*.txt")):
+   with open(newList[-1], 'rb') as file1:
+     with open(newList[-2], 'rb') as file2:
+       for x in file1.readlines():
+         for y in file2.readlines():
+           if x != y:
+              summaryInfo += str(x[1])
+              os.stat_result(
              
               
               
